869a9f886f0e4fbec95a1564edacd4d92dc6dadb
spicyuber <ilankovicaleksandar@gmail.com>
2024-07-09 21:38:47 +0200

    Stilizovana nova Cards komponenta ali fali: Implementacija fetchPlanetData i cela funkcija prebacivanja na drugu karticu.

--- veb-igrica-memorije/src/components/Cards.tsx
+++ veb-igrica-memorije/src/components/Cards.tsx
@@ -9,15 +9,31 @@ const Cards = ()=>{
 
    const [currentCard, setCurrentCard]=useState<number>(1);
    const [isFlipped,setIsFlipped]=useState<boolean>(false);
+   const [temperature, setTemperature]=useState<number|null>(null);
     const HandleFlipCard = (uid:number, forceflip:boolean)=>{
         setIsFlipped(!isFlipped);
 
     } ;
   const cardNames:string[]=["SUNČEV SISTEM", "MERKUR", "VENERA", "ZEMLJA","MARS","JUPITER", "SATURN", "URAN", "NEPTUN"];
+  const APINames:string[]=["sun", "mercury", "venus", "earth","mars","jupiter", "saturn", "uranus", "neptune"];
+
+  async function fetchPlanetData(name:string):Promise<number|null> {
+    /*IMPLEMENTIRAJ */
+    return null;
+  }
+  
+  function handleTemperature(){
+  fetchPlanetData(APINames[currentCard-1]).then(avgTemp => {
+setTemperature(avgTemp);
+  });
+}
 
 return (<>
 <div className="default-bg"></div>
 <Navbar></Navbar>
+<div className="big-transparent-block">
+<div className="big-card-title">{cardNames[currentCard-1]}</div>
+<div className="big-card">
 <OneCard
 
           uid={1}
@@ -26,6 +42,9 @@ return (<>
           isMatched={false}
           flipCard={HandleFlipCard}
 ></OneCard>
+</div>
+<div className="card-temperature">{temperature==null?("TEMPERATURA: "+temperature):""}</div>
+</div>
 </>);
 };
 
